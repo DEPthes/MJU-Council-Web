c392647106e7c37618131e8a8f33ec3aafb0c2da
si14444 <a1@1ui-MacBookAir.local>
2024-11-16 23:34:14 +0900

    프로젝트 초기 세팅

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Home = () => {
+  return <TEST>home</TEST>;
+};
+
+export default Home;
+
+const TEST = styled.div`
+  color: var(--White);
+  font: var(--BodyText);
+`;

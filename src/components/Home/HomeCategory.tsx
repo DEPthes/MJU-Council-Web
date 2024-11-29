a6e9921dd7888e65dd08383b771bafa9df2e0b30
si14444 <a1@1ui-MacBookAir.local>
2024-11-29 15:04:24 +0900

    fix: 라우팅 경로 수정

--- src/components/Home/HomeCategory.tsx
+++ src/components/Home/HomeCategory.tsx
@@ -1,10 +1,10 @@
 import * as S from "../../styles/Home/Component/HomeCategoryComponentStyle";
 
+import Book from "../../assets/image/Book.svg";
 import Bulb from "../../assets/image/Bulb.svg";
+import Calander from "../../assets/image/Calendar_duotone_line.svg";
 import Comment from "../../assets/image/Comment.svg";
 import Heart from "../../assets/image/Heart...S2.svg";
-import Calander from "../../assets/image/Calendar_duotone_line.svg";
-import Book from "../../assets/image/Book.svg";
 
 const HomeCategory = () => {
     return(
@@ -22,11 +22,11 @@ const HomeCategory = () => {
                     <S.Img src={Heart}/>
                     <S.CName>제휴 현황</S.CName>
                 </S.Indiv>
-                <S.Indiv to={"/activityReport/event"}>
+                <S.Indiv to={"/news/event"}>
                     <S.Img src={Calander}/>
                     <S.CName>행사</S.CName>
                 </S.Indiv>
-                <S.Indiv to={"/activityReport/minutes"}>
+                <S.Indiv to={"/document/minutes"}>
                     <S.Img src={Book}/>
                     <S.CName>회의록</S.CName>
                 </S.Indiv>

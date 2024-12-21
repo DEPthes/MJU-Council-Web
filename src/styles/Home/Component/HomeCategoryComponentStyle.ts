2ba1de1f7bf57958031c519a0871ae7500f567d8
rlotr02 <0314kelee@naver.com>
2024-12-22 07:59:14 +0900

    fix: 메인페이지 표시 오류 수정

--- src/styles/Home/Component/HomeCategoryComponentStyle.ts
+++ src/styles/Home/Component/HomeCategoryComponentStyle.ts
@@ -2,38 +2,37 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Div = styled.div`
-    display:flex;
-    flex-direction: row;
-    justify-content:center;
-    margin: 40px 0;
-    width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 40px 0;
+  width: 100%;
+  gap: 12px;
 `;
 
 export const Img = styled.img`
-    display: flex;
-    padding: 12px;
-    align-items: center;
-    border-radius: 100px;
-    border: 1px solid var(--M30);
-    margin: 0 6px;
-    background-color:white;
-    cursor: pointer;
+  display: flex;
+  padding: 12px;
+  align-items: center;
+  border-radius: 100px;
+  border: 1px solid var(--M30);
+  background-color: white;
+  cursor: pointer;
 `;
 
 export const Indiv = styled(Link)`
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    text-decoration: none;
-    width: 16%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-decoration: none;
+  width: 16%;
 `;
 
 export const CName = styled.p`
-    display:flex;
-    margin: 8px 0 0 0;
-    justify-content: center;
+  text-align: center;
+  margin-top: 8px;
 
-    color: var(--Primary);
-    text-align: center;
-    font: var(--NavigationTitle);
+  color: var(--Primary);
+  text-align: center;
+  font: var(--NavigationTitle);
 `;
